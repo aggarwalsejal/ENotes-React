ae97b184a499d8cbef2127ae7993562ad57ee258
sejal175 <sejalaggarwal0@gmail.com>
2023-07-20 18:07:59 +0530

    Password Hashing

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,8 +2,11 @@ const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
 const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+const jwt=require("jsonwebtoken");
 const res = require("express/lib/response");
 
+const JWT_SECRET="Nicetokenwhichwearegiving"
 //Create a user using POST
 router.post(
   "/",
@@ -23,22 +26,27 @@ router.post(
     try {
       let user = await User.findOne({ email: req.body.email });
       if (user) {
-        return res
-          .status(400)
-          .json({
-            errors: "Sorry a user with this email address is already present",
-          });
+        return res.status(400).json({
+          errors: "Sorry a user with this email address is already present",
+        });
       }
-      //create a new user
-      user = await User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-      });
-      res.json(user);
-    } 
-    //catch the errors
-    catch (error) {
+      const salt = await bcrypt.genSalt(10);
+      const secPass = await bcrypt.hash(req.body.password, salt);
+        //create a new user
+        user = await User.create({
+          name: req.body.name,
+          email: req.body.email,
+          password: secPass,
+        });
+        const data = {
+            user:{
+                id: user.id
+            }
+        }
+        const autotoken=jwt.sign(data, JWT_SECRET);
+      res.json(autotoken);
+    } catch (error) {
+      //catch the error
       console.error(error.message);
       res.status(500).send("Some Error Occured");
     }
