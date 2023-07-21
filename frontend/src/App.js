a650c3dde4f439fdfcba912307447939c1c2a354
sejal175 <sejalaggarwal0@gmail.com>
2023-07-21 19:25:37 +0530

    Login setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,7 +13,7 @@ function App() {
     <>
     <Router>
       <Navbar /> 
-      
+      <div className='container'>
       <Routes>
         <Route exact
               path="/"
@@ -23,6 +23,7 @@ function App() {
       <Routes>
         <Route exact path="/about" element={<About /> }></Route>
       </Routes>
+      </div>
     </Router>
     </>
   );
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -9,7 +9,7 @@ const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
   <div className="container-fluid">
-    <Link className="navbar-brand" to="/">Navbar</Link>
+    <Link className="navbar-brand" to="/">E-Notes</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
