9f4fea0b95513edde97d27c13e90fb3495070d37
Sejal Aggarwal <sejalaggarwal0@gmail.com>
2023-08-09 19:58:12 +0000

    udpate clusted

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const mongoURI ="mongodb+srv://$DATASOURCE_USERNAME:$DATASOURCE_PASSWORD@atlascluster.wctf8cb.mongodb.net/";
+const mongoURI = "mongodb+srv://$DATASOURCE_USERNAME:$DATASOURCE_PASSWORD@cluster0.mmejmaq.mongodb.net/";
 
 const connectToMongo = async () => {
   try {
